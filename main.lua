9dcaf78535b6334f46d75e24f6b7ae184acc6bd8
kuromienjoyer <phoebeperkins1685@gmail.com>
2025-02-26 17:51:39 +0000

    Moon and Uranus

--- main.lua
+++ main.lua
@@ -13,15 +13,18 @@ SMODS.Atlas{
     px = 142,
     py = 190
 }
--- Moon
+-- Moon DONE
 SMODS.Joker{
     key = 'Sailor Moon',
     loc_txt = {
         name = 'Sailor Moon',
         text = {
-            'Moon Desc.',
+            '1 in 2 chance create a random ',
+            '{C:dark_edition}Negative{} {C:planet}Planet{} card ',
+            'every played hand.'
         }
     },
+    rarity = 2,
     atlas = 'Jokers',
     pos = {x = 0, y = 0},
     config = { 
@@ -33,16 +36,18 @@ SMODS.Joker{
     end,
     calculate = function(self,card,context)
         if context.joker_main then
-            return {
-                card = card,
-                Xmult_mod = card.ability.extra.Xmult,
-                message = 'X' .. card.ability.extra.Xmult,
-                colour = G.C.MULT
-            }
+            if math.random(2) == 2 then
+                local card = create_card('Planet', G.consumeables, nil, nil, nil, nil, nil, 'c_moon')
+                card:set_edition('e_negative', true)
+                card:add_to_deck()
+                G.consumeables:emplace(card)
+                G.GAME.consumeable_buffer = 0
+                return true
+            end
         end
-    end
+    end,
 }
--- Mercury
+-- Mercury DONE
 SMODS.Joker{
     key = 'Sailor Mercury',
     loc_txt = {
@@ -81,7 +86,7 @@ SMODS.Joker{
         end
     end,
 }
--- Venus
+-- Venus DONE
 SMODS.Joker{
     key = 'Sailor Venus',
     loc_txt = {
@@ -121,7 +126,7 @@ SMODS.Joker{
         end
     end,
 }
--- Mars
+-- Mars DONE
 SMODS.Joker{
     key = 'Sailor Mars',
     loc_txt = {
@@ -162,7 +167,7 @@ SMODS.Joker{
         end
     end,
 }
--- Jupiter
+-- Jupiter DONE
 SMODS.Joker{
     key = 'Sailor Jupiter',
     loc_txt = {
@@ -202,14 +207,14 @@ SMODS.Joker{
         end
     end,
 }
--- Saturn
-SMODS.Joker{
+-- Saturn DONE
+SMODS.Joker{ 
     key = 'Sailor Saturn',
     loc_txt = {
         name = 'Sailor Saturn',
         text = {
             'Gains {X:red,C:white}X0.2{} Mult every time you use {C:planet}Saturn{}.',
-            '{C:inactive}(Currently {X:red,C:white}X#1#{} Mult)',
+            '{C:inactive}(Currently {X:red,C:white}X#1#{}{C:inactive} Mult)',
         }
     },
     rarity = 3,
@@ -250,42 +255,54 @@ SMODS.Joker{
         end
     end,
 }
--- Uranus
+-- Uranus DONE
 SMODS.Joker{
     key = 'Sailor Uranus',
     loc_txt = {
         name = 'Sailor Uranus',
         text = {
-            'Uranus Desc.',
+            'Gains {C:red}+12{} Mult every time you use {C:planet}Uranus{}.',
+            '{C:inactive}(Currently {C:red}+#1#{} {C:inactive}Mult){}',
         }
     },
+    rarity = 2,
     atlas = 'Jokers',
     pos = {x = 0, y = 0},
     config = { 
         extra = {
+            mult = 0,  -- Set to 1
+            Xmult = 1,
+
         }
     },
     loc_vars = function(self, info_queue, center)
-        return {vars = {center.ability.extra.Xmult}}
+        return {vars = {center.ability.extra.mult}}
     end,
-    calculate = function(self,card,context)
+    calculate = function(self, card, context)
+        if context.using_consumeable and context.consumeable.ability.name == "Uranus" and not context.blueprint then
+            card.ability.extra.mult = card.ability.extra.mult + 12
+            return {
+                message = 'Upgraded!',
+                colour = G.C.RED,
+            }
+        end
         if context.joker_main then
             return {
                 card = card,
-                Xmult_mod = card.ability.extra.Xmult,
-                message = 'X' .. card.ability.extra.Xmult,
+                mult_mod = card.ability.extra.mult,
+                message = '+' .. card.ability.extra.mult,
                 colour = G.C.MULT
             }
         end
-    end
+    end,
 }
--- Neptune
+-- Neptune TODO
 SMODS.Joker{
-    key = 'Sailor Moon',
+    key = 'Sailor Neptune',
     loc_txt = {
-        name = 'Sailor Moon',
+        name = 'Sailor Neptune',
         text = {
-            'Moon Desc.',
+            'Neptune Desc.',
         }
     },
     atlas = 'Jokers',
@@ -308,7 +325,7 @@ SMODS.Joker{
         end
     end
 }
--- Pluto
+-- Pluto DONE
 SMODS.Joker{
     key = 'Sailor Pluto',
     loc_txt = {
